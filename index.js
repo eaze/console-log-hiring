a8c73580831aa22bd6485c759fe6a7c1ad26490a
Marco Sgrignuoli <marco@eazeup.com>
2016-06-30 17:04:59 -0700

    Update careers page url

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 module.exports = logHiring
 
 function logHiring () {
-  console.info('Eaze is hiring! https://angel.co/eaze/jobs 🚀')
+  console.info('Eaze is hiring! https://www.eazeup.com/careers 🚀')
 }
